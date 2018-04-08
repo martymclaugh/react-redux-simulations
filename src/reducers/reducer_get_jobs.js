0605d45a7c73f6661052a636004d093ef12b28dd
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 14:38:06 -0700

    add job from create job response

--- src/reducers/reducer_get_jobs.js
+++ src/reducers/reducer_get_jobs.js
@@ -6,10 +6,12 @@ import {
 export default function( state = [], action) {
   switch (action.type) {
     case types.FETCH_ALL_JOBS:
+      return action.payload.data.jobs;
+    case types.CREATE_JOB:
       return [
         ...state,
-        ...action.payload.data.jobs,
-      ]
+        action.payload.data,
+      ];
     default:
       return state;
   }
