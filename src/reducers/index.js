2a79a30f640ec938b7983eb35bfb8f615b91811f
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 12:46:34 -0700

    add jobs and job reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import getSoftwareReducer from './reducer_get_software';
+import getJobsReducer from './reducer_get_jobs';
+import getJobReducer from './reducer_get_job';
 
 const rootReducer = combineReducers({
   software: getSoftwareReducer,
+  // hardware: getHardwareReducer,
+  jobs: getJobsReducer,
+  job: getJobReducer,
 });
 
 export default rootReducer;
