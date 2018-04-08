f0c8d143c8a51203991fffaa595cc0c92dc421e1
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 14:37:45 -0700

    add hardware reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import getSoftwareReducer from './reducer_get_software';
+import getHardwareReducer from './reducer_get_hardware';
 import getJobsReducer from './reducer_get_jobs';
 import getJobReducer from './reducer_get_job';
 
 const rootReducer = combineReducers({
   software: getSoftwareReducer,
-  // hardware: getHardwareReducer,
+  hardware: getHardwareReducer,
   jobs: getJobsReducer,
   job: getJobReducer,
 });
