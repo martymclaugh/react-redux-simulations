31abcba8863eeba831b9261dd377150c79c111f8
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 14:36:12 -0700

    add software hardware and create job actions and action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,9 @@ const ROOT_URL = 'http://localhost:3000/api';
 export const types = {
   FETCH_ALL_JOBS: 'FETCH_ALL_JOBS',
   FETCH_JOB: 'FETCH_JOB',
+  FETCH_ALL_SOFTWARE: 'FETCH_ALL_SOFTWARE',
+  FETCH_ALL_HARDWARE: 'FETCH_ALL_HARDWARE',
+  CREATE_JOB: 'CREATE_JOB',
 }
 
 export const fetchAllJobs = () => {
@@ -15,11 +18,39 @@ export const fetchAllJobs = () => {
     payload: request,
   }
 }
+
 export const fetchJob = (jobId) => {
   const request = axios.get(`${ROOT_URL}/jobs/${jobId}`);
 
   return {
     type: types.FETCH_JOB,
+    payload: request,
+  }
+}
+
+export const fetchAllSoftware = () => {
+  const request = axios.get(`${ROOT_URL}/software`);
+
+  return {
+    type: types.FETCH_ALL_SOFTWARE,
+    payload: request,
+  }
+}
+
+export const fetchAllHardware = () => {
+  const request = axios.get(`${ROOT_URL}/hardware`);
+
+  return {
+    type: types.FETCH_ALL_HARDWARE,
+    payload: request,
+  }
+}
+
+export const createJob = (job) => {
+  const request = axios.post(`${ROOT_URL}/create`, job);
+
+  return {
+    type: types.CREATE_JOB,
     payload: request,
   }
 }
