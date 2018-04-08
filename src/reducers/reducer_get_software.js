e9b77161baa9ae6fea42dfa76d4b7215f3439bf5
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 12:45:48 -0700

    change structure of action types

--- src/reducers/reducer_get_software.js
+++ src/reducers/reducer_get_software.js
@@ -1,11 +1,9 @@
-import {
-  GET_SOFTWARE,
-} from '../actions/index';
+import { types } from '../actions/index';
 
 //reducer for getting software
 export default function( state = [], action) {
   switch (action.type) {
-    case GET_SOFTWARE:
+    case types.GET_SOFTWARE:
       return action.payload.data.options;
     default:
       return state;
