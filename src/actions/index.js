cee88cf9bbade1d399f6e05842bb835b34c79e01
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 12:44:21 -0700

    add fetch jobs and fetch job action creators and types

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,24 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://localhost:3000/api';
 
+export const types = {
+  FETCH_ALL_JOBS: 'FETCH_ALL_JOBS',
+  FETCH_JOB: 'FETCH_JOB',
+}
+
+export const fetchAllJobs = () => {
+  const request = axios.get(`${ROOT_URL}/jobs`);
+
+  return {
+    type: types.FETCH_ALL_JOBS,
+    payload: request,
+  }
+}
+export const fetchJob = (jobId) => {
+  const request = axios.get(`${ROOT_URL}/jobs/${jobId}`);
+
+  return {
+    type: types.FETCH_JOB,
+    payload: request,
+  }
+}
