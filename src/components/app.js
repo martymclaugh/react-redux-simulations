4456c867db3007290c0564660ea8f9da47d3592e
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 12:44:39 -0700

    add job and jobs components, react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import JobsList from './JobsList';
+import Job from './Job';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-  }  
+  }
 
   render() {
     return (
-      <div className="fe-app">
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={JobsList} />
+          <Route path="/jobs/:id" component={Job} />
+        </Switch>
+      </Router>
     );
   }
 }
