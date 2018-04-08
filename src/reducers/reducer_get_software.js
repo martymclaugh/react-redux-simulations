d816857b8b43843cd0c1f92a10f92021335b14da
Marty McLaughlin <martymclaugh@gmail.com>
2018-04-08 14:38:20 -0700

    change name

--- src/reducers/reducer_get_software.js
+++ src/reducers/reducer_get_software.js
@@ -3,7 +3,7 @@ import { types } from '../actions/index';
 //reducer for getting software
 export default function( state = [], action) {
   switch (action.type) {
-    case types.GET_SOFTWARE:
+    case types.FETCH_ALL_SOFTWARE:
       return action.payload.data.options;
     default:
       return state;
